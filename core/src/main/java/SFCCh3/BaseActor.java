c4440db9a20b418b96e6384c5b0c288358c8edc4
lemax97 <lemax97@mail.ru>
2020-09-09 22:46:42 +0300

    - working animation - turtle swimming

--- core/src/main/java/SFCCh3/BaseActor.java
+++ core/src/main/java/SFCCh3/BaseActor.java
@@ -13,12 +13,21 @@ import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Animation.PlayMode;
 import com.badlogic.gdx.graphics.g2d.Batch;
 
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.MathUtils;
+
 
 public class BaseActor extends Actor {
 
     private Animation<TextureRegion> animation;
     private float elapsedTime;
     private boolean animationPaused;
+
+    private Vector2 velocityVec;
+    private Vector2 accelerationVec;
+    private float acceleration;
+    private float maxSpeed;
+    private float deceleration;
 
     public BaseActor(float x, float y, Stage stage) {
 
@@ -34,8 +43,98 @@ public class BaseActor extends Actor {
         elapsedTime = 0;
         animationPaused = false;
 
+        // initialize physics
+        velocityVec = new Vector2(0,0);
+
+        accelerationVec = new Vector2(0,0);
+        acceleration = 0;
+        maxSpeed = 1000;
+        deceleration = 0;
+
     }
 
+    public void setSpeed(float speed){
+
+        //length is zero, then assume motion angle is zero degrees
+        if (velocityVec.len() == 0)
+            velocityVec.set(speed, 0);
+        else
+            velocityVec.setLength(speed);
+    }
+
+    public float getSpeed(){
+
+        return velocityVec.len();
+    }
+
+    public void setMotionAngle(float angle){
+
+        velocityVec.setAngle(angle);
+    }
+
+    public float getMotionAngle(){
+
+        return velocityVec.angle();
+    }
+
+    public boolean isMoving(){
+
+        return (getSpeed() > 0);
+    }
+
+    public void setAcceleration(float acc){
+
+        acceleration = acc;
+    }
+
+    public void accelerateAtAngle(float angle){
+
+        accelerationVec.add(new Vector2(acceleration,0).setAngle(angle));
+    }
+
+    public void accelerateForward(){
+
+        accelerateAtAngle( getRotation());
+    }
+
+    public void setMaxSpeed(float ms){
+
+        maxSpeed = ms;
+    }
+
+    public void setDeceleration(float dec){
+
+        deceleration = dec;
+    }
+
+    public void applyPhysics(float dt){
+
+        // apply acceleration
+        velocityVec.add( accelerationVec.x * dt, accelerationVec.y * dt);
+
+        float speed = getSpeed();
+
+        // decrease speed (decelerate) when not accelerating
+        if (accelerationVec.len() == 0)
+            speed -= deceleration * dt;
+
+        // keep speed within set bounds
+        speed = MathUtils.clamp(speed, 0, maxSpeed);
+
+        // update velocity
+        setSpeed(speed);
+
+        // apply velocity
+        moveBy(velocityVec.x * dt, velocityVec.y * dt);
+
+        // reset acceleration
+        accelerationVec.set(0, 0);
+    }
+
+    /**---------------
+    **      Animation
+     **----------------
+     **/
 
     public void setAnimation(Animation<TextureRegion> anim){
 
--- core/src/main/java/SFCCh3/Turtle.java
+++ core/src/main/java/SFCCh3/Turtle.java
@@ -1,8 +1,11 @@
 package SFCCh3;
 
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 
 public class Turtle extends BaseActor {
+
     public Turtle(float x, float y, Stage stage) {
         super(x, y, stage);
 
@@ -10,5 +13,30 @@ public class Turtle extends BaseActor {
                                 "assets/turtle-4.png", "assets/turtle-5.png", "assets/turtle-6.png"};
 
         loadAnimationFromFiles(filenames, 0.1f, true);
+
+        setAcceleration(400);
+        setMaxSpeed(100);
+        setDeceleration(50);
+    }
+
+    public void act(float dt){
+
+        super.act(dt);
+
+        if (Gdx.input.isKeyPressed(Keys.LEFT))
+            accelerateAtAngle(180);
+        if (Gdx.input.isKeyPressed(Keys.RIGHT))
+            accelerateAtAngle(0);
+        if (Gdx.input.isKeyPressed(Keys.UP))
+            accelerateAtAngle(90);
+        if (Gdx.input.isKeyPressed(Keys.DOWN))
+            accelerateAtAngle(270);
+
+        applyPhysics(dt);
+
+        setAnimationPaused(!isMoving() || !Gdx.input.isKeyPressed(Keys.ANY_KEY));
+
+        if (getSpeed() > 0)
+            setRotation(getMotionAngle());
     }
 }
